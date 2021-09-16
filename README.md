e812f23db900c66283c35e415d69be8612f50f2c
kelsimhoyle <kelsimhoyle@gmail.com>
2021-09-16 12:25:03 -0600

    completed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 
 # [Timestamp Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice)
+
+Refresh/Review of Node and Express while going through the freeCodeCamp Backend Development and APIs.
--- server.js
+++ server.js
@@ -2,31 +2,60 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: 'hello API' });
 });
 
 
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.get("/api/:date?", (req, res) => {
+  const unixTimestamp = parseInt(req.params.date * 1);
+  let date;
+  let response;
+
+  if (req.params.date) {
+    if (isNaN(unixTimestamp)) {
+      date = new Date(req.params.date)
+    } else {
+      date = new Date(unixTimestamp)
+    }
+
+  } else {
+    date = new Date(Date.now());
+  }
+
+
+  if (date == "Invalid Date") {
+    response = {
+      error: "Invalid Date"
+    }
+  } else {
+    response = {
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    }
+  }
+
+  res.json(response);
 });
